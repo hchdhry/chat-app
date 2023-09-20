1f885fb1810e21f58d3837ca86fd9f6345d78dd7
hassan <hchoudhury20@hotmail.com>
2023-09-20 13:20:26 +0100

    render data from api request

--- front-end/src/chats.jsx
+++ front-end/src/chats.jsx
@@ -1,20 +1,28 @@
-import { React,useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const Chats = () => {
+  const [chats, setChats] = useState([]);
   const fetchdata = async () => {
-    const data = await axios.get("http://localhost:3000/api/chats");
+    const {data} = await axios.get("http://localhost:3000/api/chats");
     console.log(data);
+    setChats(data);
   
   }
   useEffect(() => {
     fetchdata();
   }, []);
+ 
   return (
     <>
-      <h1>Chats</h1>  
-    </>
+    <div>
+      {chats.map(chat => (
+        <div>{chat.chatName}</div>
+      ))}
+    </div>
+  </>
   );
   }
 
 export default Chats;
+
