7ac9c705e41bb02193b4ff7c19b00c092bfea095
hassan <hchoudhury20@hotmail.com>
2023-09-21 22:44:41 +0100

    create controller to allow users to sign up

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,19 +2,13 @@ var express = require('express');
 var router = express.Router();
 const chats = require("../data");
 const cors = require('cors');
+const registerUser = require("../controllers/userController");
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get("/api/chats", cors(), function(req, res, next) {
-  res.send(chats);
-}); 
-router.get("/api/chats/:id", cors(), function(req, res, next) {
-  const singlechat=chats.find(chat=>chat._id===req.params.id);
-    res.send(singlechat)
-  
-  
-}); 
+router.post('/',registerUser); 
+
 
 module.exports = router;
