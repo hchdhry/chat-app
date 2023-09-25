abb283e2ae38c3f5eaabfef97a061e5a0e69cb4d
hassan <hchoudhury20@hotmail.com>
2023-09-25 22:48:18 +0100

    implement jwt authorisation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 
+
 const registerUser = asyncHandler(async (req, res) => {
     const { name, email, password,pic } = req.body;
     if (!name || !email || !password) {
