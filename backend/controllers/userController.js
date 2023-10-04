889be0898746f2dc4590aa67d75ffb30b293eae0
hassan <hchoudhury20@hotmail.com>
2023-10-04 22:26:58 +0100

    create controllers and routes for chat functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -34,22 +34,14 @@ const registerUser = asyncHandler(async (req, res) => {
           } 
       
 });
-const authUser = asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
 
-  const user = await User.findOne({ email });
-
-  if (user && (await user.matchPassword(password))) {
-    res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      isAdmin: user.isAdmin,
-      token: generateToken(user._id),
-    });
-  } else {
-    res.status(401);
-    throw new Error("Invalid Email or Password");
-  }
+const allUsers = asyncHandler(async (req, res) => {
+  const keyword = req.query.search && {
+    $or: [
+      { name: { $regex: req.query.search, $options: 'i' } },
+      { email: { $regex: req.query.search, $options: 'i' } },
+    ],
+  };
 });
-module.exports = {authUser,registerUser};
+
+module.exports = {registerUser,allUsers};
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,16 +2,14 @@ var express = require('express');
 var router = express.Router();
 const chats = require("../data");
 const cors = require('cors');
-const {authUser,registerUser} = require("../controllers/userController");
+const {registerUser,allUsers} = require("../controllers/userController");
 const passport = require('../passport');
 
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
-router.post('/',cors(),registerUser); 
+
+router.post('/', cors(), registerUser);
+router.get('/', allUsers); 
 router.use(cors());
 router.get('/chats',cors(),(req,res)=>{
   res.send("yee");
